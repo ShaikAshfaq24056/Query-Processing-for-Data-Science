d650753981a098aec8832286e1d41ddfad5c733f
Shaik Ashfaq <113223950+ShaikAshfaq24056@users.noreply.github.com>
2023-11-02 08:47:47 +0530

    Delete 1_Data_Wrangling.py

--- 1_Data_Wrangling.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data = {'Name': ['Jai', 'Princi', 'Gaurav','Anuj', 'Ravi', 'Natasha', 'Riya'],
-		'Age': [17, 17, 18, 17, 18, 17, 17],
-		'Gender': ['M', 'F', 'M', 'M', 'M', 'F', 'F'],
-		'Marks': [90, 76, 'NaN', 74, 65, 'NaN', 71]}
-df = pd.DataFrame(data)
-print(df)
